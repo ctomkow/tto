b4a13f8da9d2a1fddc77775a7c817687ad559a87
Craig Tomkow <ctomkow@gmail.com>
2019-07-17 15:38:22 -0700

    initial work. wip mysqldump with restore using go mysql driver

---
+++ mysql.go
@@ -0,0 +1,133 @@
+package main
+
+import (
+	"bufio"
+	"database/sql"
+	"strings"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"time"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func connectToDatabase (dbPort string, dbIp string, dbUser string, dbPass string, dbName string) (db *sql.DB) {
+
+	// prep DB connection
+	db, err := sql.Open("mysql", dbUser + ":" + dbPass + "@tcp(" + dbIp + ":" + dbPort + ")/" + dbName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = db.Ping()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return db
+}
+
+
+func dropDatabase (db *sql.DB, dbName string) {
+
+	_, err := db.Exec("DROP DATABASE " + dbName + ";")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func createDatabase (db *sql.DB, dbName string) {
+
+	_, err := db.Exec("CREATE DATABASE " + dbName + ";")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func dumpDatabase(dbPort string, dbIp string, dbUser string, dbPass string, dbName string) string {
+
+	// YYYYMMDDhhmmss
+	currentTime := time.Now().Format("20060102150405")
+
+	portArg := "-P" + dbPort
+	ipArg   := "-h" + dbIp
+	userArg := "-u" + dbUser
+	passArg := "-p" + dbPass
+	sqlFile := dbName + currentTime + ".sql"
+
+	cmd := exec.Command("mysqldump", "--single-transaction", "--routines", "--triggers", portArg, ipArg, userArg, passArg, dbName)
+	stdout, err := cmd.StdoutPipe()
+	defer stdout.Close()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = cmd.Start()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	bytes, err := ioutil.ReadAll(stdout)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = ioutil.WriteFile(sqlFile, bytes, 0644)
+	return sqlFile
+}
+
+func restoreDatabase(db *sql.DB, dump string) {
+
+	// read .sql statement by statement and fire off to db server
+	// NOTE: bufio.NewScanner has a line length limit of 65536 chars. mysqldump does only one INSERT per table. Not good!
+	//       Using ReadString with a ';' delimiter, ensuring that the next character after is '\n'
+	fd, err := os.Open(dump)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer fd.Close()
+
+	reader := bufio.NewReader(fd)
+	var buffer strings.Builder
+
+	// loop and send queries until EOF
+	for {
+
+		statement, err := reader.ReadString(';')
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+		}
+
+		buffer.WriteString(statement)
+
+		// look at next byte
+		oracleByte, err := reader.Peek(1)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		//fmt.Print(buffer.String())
+		//fmt.Printf("%q", oracleByte)
+
+		// if next byte is '\n', then sql statement looks complete
+		if oracleByte[0] == 10 {
+			_, err = db.Exec(buffer.String())
+			if err != nil {
+				log.Fatal(err)
+			}
+			buffer.Reset()
+		} else {
+			continue
+		}
+	}
+
+}
--- tto.go
+++ tto.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"log"
 	"os"
+	"time"
 )
 import "fmt"
 
@@ -12,19 +13,21 @@ type config struct {
 	System struct {
 		Role      string `json:"role"`
 		Replicate struct {
-			Mysql string `json:"mysql"`
+			Mysql    string `json:"mysql"`
+			Interval string `json:"interval"`
 		}
 	}
 	Mysql struct {
-		DBip   string `json:"db_ip"`
 		DBport string `json:"db_port"`
-		DBname string `json:"db_name"`
+		DBip   string `json:"db_ip"`
 		DBuser string `json:"db_user"`
 		DBpass string `json:"db_pass"`
+		DBname string `json:"db_name"`
 	}
 }
 
 func init() {
+
 	fmt.Println("### ", "three-two-one sync and backup!")
 	fmt.Println("### ", "version 0.01")
 	fmt.Println("###")
@@ -35,21 +38,35 @@ func main() {
 	// parse cli flag input
 	configFile := cliFlags()
 
+	fmt.Println(time.Now().Format(time.RFC850))
+
 	// parse config file input
 	config, _ := loadConfig(*configFile)
 
-	fmt.Println(config.System.Replicate.Mysql)
+	// db connection
+	db := connectToDatabase(config.Mysql.DBport, config.Mysql.DBip, config.Mysql.DBuser, config.Mysql.DBpass, config.Mysql.DBname)
+
+	// dump DB, return dump file name
+	mysqlDump := dumpDatabase(config.Mysql.DBport, config.Mysql.DBip, config.Mysql.DBuser, config.Mysql.DBpass, config.Mysql.DBname)
+
+	// TODO: send dump to remote receiver
+
+	// TODO: if remote receiver, get dump to restore
+
+	// restore DB
+	restoreDatabase(db, mysqlDump)
 }
 
 // parse -conf flag and return as pointer
 func cliFlags() *string {
 
-	confFilePtr := flag.String("conf", "conf.json", "name of conf file. default name is conf.json")
+	confFilePtr := flag.String("conf", "conf.json", "name of conf file.")
 	flag.Parse()
 	return confFilePtr
 }
 
 func loadConfig(filename string) (config, error) {
+
 	var configStruct config
 	fd, err := os.Open(filename)
 	defer fd.Close() // close fd when function returns
@@ -62,5 +79,3 @@ func loadConfig(filename string) (config, error) {
 	err = jsonParser.Decode(&configStruct)
 	return configStruct, err
 }
-
-//func dumpDatabase(dbIP string, dbPort string, )
